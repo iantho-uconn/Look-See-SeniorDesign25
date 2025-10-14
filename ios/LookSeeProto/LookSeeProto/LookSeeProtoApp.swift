bfff184fae5109b7a8398ae73ac5fee809962e63
idt22002 <ian.thompson@uconn.edu>
2025-10-14 14:47:10 -0400

    added ios folder and basic projet template that we can create branches off of

---
+++ ios/LookSeeProto/LookSeeProto/LookSeeProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LookSeeProtoApp.swift
+//  LookSeeProto
+//
+//  Created by Ian Thompson on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct LookSeeProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/LookSeeProto/LookSeeProtoTests/LookSeeProtoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LookSeeProtoTests.swift
+//  LookSeeProtoTests
+//
+//  Created by Ian Thompson on 10/14/25.
+//
+
+import Testing
+@testable import LookSeeProto
+
+struct LookSeeProtoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
