bdda7f921dc6e9f39cc0ddd34e757a409002d68e
ChristianKB2003 <christian.barbara@uconn.edu>
2026-03-24 16:03:32 -0400

    Show images

--- ios/LookSeeProto/UI Files/PromotionEditor.swift
+++ ios/LookSeeProto/UI Files/PromotionEditor.swift
@@ -9,13 +9,15 @@ import SwiftUI
 import PhotosUI
 
 struct PromotionEditor: View {
-    @State private var businesses = ["Test", "Jerry's Bait Shop", "Hardware Store"]
+    @State private var businesses = ["Dick's Automotive", "Jerry's Bait Shop", "Hardware Store"] // Set up API to retrieve respective businesses of owner
     @State private var selectedBusiness = String()
     @State private var promoName = String()
     @State private var promoDescription = String()
     @State private var startDate = Date()
     @State private var endDate = Date()
     @State var selectedItems: [PhotosPickerItem] = []
+    @State private var media: [Image] = []
+    @State private var submit = false
     var body: some View {
         VStack {
             Form {
@@ -47,20 +49,46 @@ struct PromotionEditor: View {
                     PhotosPicker(selection: $selectedItems) {
                         Text("Add media")
                     }
-                        .onChange(of: selectedItems){}
+                        .onChange(of: selectedItems){_, newValue in
+                            media.removeAll()
+                            
+                            newValue.forEach({ selectedItem in
+                                Task {
+                                    if let imageData = try? await selectedItem.loadTransferable(type: Data.self),
+                                       let uiImage = UIImage(data: imageData){
+                                        media.append(Image(uiImage: uiImage))
+                                    } else {
+                                        print("Image Error")
+                                    }
+                                }
+                            })
+                        }
                 } header: {Text("Promotion Media")}
-                MediaList(selectedItems: $selectedItems)
+                MediaList(selectedItems: $selectedItems, media: $media)
             }
+            Button("Submit", role: .cancel) {submit = true}
+                .buttonStyle(.bordered)
         }
     }
 }
 
 struct MediaList: View {
     @Binding var selectedItems: [PhotosPickerItem]
+    @Binding var media: [Image]
     var body: some View {
         if selectedItems.isEmpty {
             Text("No media selected")
         } else {
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(0..<media.count, id:\.self){
+                        item in media[item]
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 300, height: 300)
+                    }
+                }
+            }
         }
     }
 }
