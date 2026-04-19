a62e2222c75c76551c41d2a59e918de84876242d
ChristianKB2003 <christiankb2003@gmail.com>
2026-04-19 15:57:19 -0400

    Hard-coded Husky Statue info

--- ios/LookSeeProto/Detection/CameraPreview.swift
+++ ios/LookSeeProto/Detection/CameraPreview.swift
@@ -262,8 +262,11 @@ struct CameraPreview: UIViewRepresentable {
             if overlay!.frame.contains(tapLocation) && !overlay!.detections.isEmpty{
                 //TODO: Add API calls to get landmark infomation from the database
                 print("Positive")
-                infoView.landmarkName = overlay!.detections[0].label
+//                infoView.landmarkName = overlay!.detections[0].label
+                infoView.landmarkName = "Husky Dog Statue"
+                infoView.landmarkDescription = "The Husky Dog Statue was completed in the spring of 1995. It was modeled after the then-current mascot of the University of Connecticut, Jonathan X."
                 infoView.landmarkConfidence = (overlay!.detections[0].confidence * 100)
+                infoView.landmarkCategory = "Statue"
                 infoView.infoView.toggle()
             }
             else { print("Negative")
